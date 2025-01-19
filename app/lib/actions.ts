a34f458b880673dc2eb089465e35be035b89e8a7
hk-pG <hukada.gogotea@gmail.com>
2025-01-19 23:18:28 +0900

    update invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -36,3 +36,26 @@ export async function createInvoices(formData: FormData) {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  };
+
+  const { customerId, amount, status } = UpdateInvoice.parse(rawFormData);
+
+  const amountInCents = amount * 100;
+
+  await sql`
+  UPDATE invoices
+  SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+  WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
